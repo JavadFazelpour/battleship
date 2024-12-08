934bdb00284b131266b3bf4e93612ceeb7082b25
javad-fp <javad_fazelpour@outlook.com>
2024-12-08 15:16:41 +0330

    Add a helper method for input validation

--- battleship.js
+++ battleship.js
@@ -50,3 +50,25 @@ let model = {
     return (!ship.hits.includes(""));
   }
 };
+
+function parseGuess(guess) {
+  const alphabet = ["A", "B", "C", "D", "E", "F", "G"];
+
+  if (guess === null || guess.length !== 2) {
+    alert("Oops, please enter a letter and a number on the board.");
+  } else {
+    let firstChar = guess.charAt(0);
+    let row = alphabet.indexOf(firstChar);
+    let column = guess.charAt(1);
+    if (isNaN(column)) {
+      alert("Oops, that isn't on the board.");
+    }
+    else if (row < 0 || row >= model.boardSize || column < 0 || column >= model.boardSize) {
+      alert("Oops, that's off the board!");
+    }
+    else {
+      return row + column;
+    }
+  }
+  return null;
+}
